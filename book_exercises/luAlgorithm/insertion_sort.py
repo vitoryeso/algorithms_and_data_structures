49513b6a65942b782a07594fcf2964f286ed51ba
vitoryeso <vitoryeso@outlook.com>
2020-06-22 18:44:25 -0300

    add modified merge_sort using insertion for tiny sort

--- book_exercises/luAlgorithm/insertion_sort.py
+++ book_exercises/luAlgorithm/insertion_sort.py
@@ -1,5 +1,5 @@
-def insertion_sort(A):
-    for j in range(1, len(A)):
+def insertion_sort(A, p, r):
+    for j in range(p, r  + 1):
         key = A[j]
         i = j - 1
         while key < A[i] and i >= 0:
--- book_exercises/main.py
+++ book_exercises/main.py
@@ -1,31 +1,51 @@
-from luAlgorithm import binary_search, merge_sort, selection_sort, insertion_sort, recursive_insertion_sort, match_sum
+from copy import copy
+from luAlgorithm import binary_search, merge_sort, selection_sort, insertion_sort, recursive_insertion_sort, match_sum, merge
 
-a = [1,4,3,56,7,34,3,6,2,3,5,11,2]
-print("a: ", a)
-insertion_sort(a)
-print("a sorted with insertion: ", a, end="\n\n")
+k = 4
 
-a = [1,4,3,56,7,34,3,6,2,3,5,11,2]
-print("a: ", a)
-recursive_insertion_sort(a, len(a) - 1)
-print("a sorted with recursive insertion: ", a, end="\n\n")
+def merge_modified(A, p, r):
+    if r > p + 1:
+        q = int((r + p) / 2)
+        merge_modified(A, p, q)
+        merge_modified(A, q + 1, r)
+        global k
+        if (r - p) < k:
+            insertion_sort(A, p, q)
+            insertion_sort(A, q + 1, r) 
+        else:
+            merge.merge(A, p, q, r)
+ 
+A = [1,4,3,56,7,34,3,6,2,3,5,11,2]
+print("A: ", A)
+insertion_sort(A, 0, len(A) - 1)
+print("A sorted with insertion: ", A, end="\n\n")
 
-a = [1,4,3,56,7,34,3,6,2,3,5,11,2]
-print("a: ", a)
-selection_sort(a)
-print("a sorted with selection: ", a, end="\n\n")
+A = [1,4,3,56,7,34,3,6,2,3,5,11,2]
+print("A: ", A)
+recursive_insertion_sort(A, len(A) - 1)
+print("A sorted with recursive insertion: ", A, end="\n\n")
 
-a = [1,4,3,56,7,34,3,6,2,3,5,11,2]
-print("a: ", a)
-merge_sort(a, 0, len(a) - 1)
-print("a sorted with merge: ", a, end="\n\n")
+A = [1,4,3,56,7,34,3,6,2,3,5,11,2]
+print("A: ", A)
+selection_sort(A)
+print("A sorted with selection: ", A, end="\n\n")
 
-print("a: ", a)
-i = binary_search(a, 0, len(a) - 1, 6)
-print("search index of 56 value using binary_search: ", i, end="\n\n")
+A = [1,4,3,56,7,34,3,6,2,3,5,11,2]
+print("A: ", A)
+merge_sort(A, 0, len(A) - 1)
+print("A sorted with merge: ", A, end="\n\n")
+
+A = [1,4,3,56,7,34,3,6,2,3,5,11]
+print("A: ", A)
+merge_modified(A, 0, len(A) - 1)
+print("A sorted with merge_modified: ", A, end="\n\n")
+
+print("A: ", A)
+i = binary_search(A, 0, len(A) - 1, 6)
+print("search index of 6 value using binary_search: ", i, end="\n\n")
 
 print("exist A[i] + A[j] == x, where x is: ", 8)
-print("yes" if match_sum(a, len(a) - 1, 8) else "no")
+print("yes" if match_sum(A, len(A) - 1, 8) else "no")
 
 print("exist A[i] + A[j] == x, where x is: ", 0)
-print("yes" if match_sum(a, len(a) - 1, 0) else "no")
+print("yes" if match_sum(A, len(A) - 1, 0) else "no")
