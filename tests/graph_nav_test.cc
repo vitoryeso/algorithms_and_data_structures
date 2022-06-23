076c42bc2916c2c171467fb6f098903903c9e566
vitoryeso <vitoryeso@outlook.com>
2022-06-23 19:48:09 -0300

    add bfs tests

--- tests/graph_nav_test.cc
+++ tests/graph_nav_test.cc
@@ -2,6 +2,8 @@
 #include "../data_structures/cpp/grafo_navegacao.cpp"
 using namespace std;
 
+using namespace std;
+
 class GrafoListaAdjNavegacaoTest : public ::testing::Test {
 protected:
 	virtual void TearDown() {
@@ -48,7 +50,7 @@ void construirGrafoNaoPonderadoDirecionado(GrafoListaAdj* grafo) {
 	//v8 nao tem arestas partindo dele
 	grafo->inserirArestaDirecionada("v9", "v8");
 	grafo->inserirArestaDirecionada("v9", "v9");
-	
+
 }
 
 /* Funcao auxiliar para adicionar vertices em uma sequencia.
@@ -66,15 +68,15 @@ void inserirVertices(GrafoListaAdj* grafo, int ini, int fim) {
 
 TEST_F(GrafoListaAdjNavegacaoTest, haCaminho) {
 	//inserimos de v1 a v10, e v10 ficarah desconectado do componente principal
-	inserirVertices(grafo,1,10);	
-	construirGrafoNaoPonderadoDirecionado(grafo);		
-		
+	inserirVertices(grafo,1,10);
+	construirGrafoNaoPonderadoDirecionado(grafo);
+
 	EXPECT_TRUE(grafo->haCaminho("v1","v2"));
 	EXPECT_TRUE(grafo->haCaminho("v1","v4"));
 	EXPECT_TRUE(grafo->haCaminho("v6", "v9"));
 	EXPECT_TRUE(grafo->haCaminho("v9", "v9"));
 
-	//note que v10 nao estah conectado	
+	//note que v10 nao estah conectado
 	//logo, não existe caminhos chegando ou partindo de v10
 	//embora nao exista v10 no grafo da url descrita, nos adicionamos v10 (linha 50)
 	EXPECT_FALSE(grafo->haCaminho("v1","v10"));
@@ -89,10 +91,10 @@ TEST_F(GrafoListaAdjNavegacaoTest, colorirGrafo1Comp) {
 	//esse grafo tem apenas um componente
 	inserirVertices(grafo,1,9);
 	construirGrafoNaoPonderadoNaoDirecionado(grafo);
-	
+
 	EXPECT_EQ(grafo->colorir(), 1);
 	for (int i = 1; i <= 8; i++) {
-		//depois de colorir o grafo, todos os rotulos 
+		//depois de colorir o grafo, todos os rotulos
 		//dos vertices devem estar iguais
 		EXPECT_EQ(grafo->getVertices().at(i - 1), grafo->getVertices().at(i));
 	}
@@ -132,7 +134,7 @@ void construirGrafoCom5Componentes(GrafoListaAdj* grafo) {
 
 TEST_F(GrafoListaAdjNavegacaoTest, colorirGrafo5Comp) {
 	inserirVertices(grafo, 0, 17);
-	construirGrafoCom5Componentes(grafo);	
+	construirGrafoCom5Componentes(grafo);
 
 	//verifico que o grafo tem 5 componentes
 	EXPECT_EQ(grafo->colorir(), 5);
@@ -172,4 +174,122 @@ TEST_F(GrafoListaAdjNavegacaoTest, colorirGrafo5Comp) {
 	EXPECT_NE(grafo->getVertices().at(12), corComp2);
 	EXPECT_NE(grafo->getVertices().at(12), corComp3);
 	EXPECT_NE(grafo->getVertices().at(12), corComp4);
+}
+
+TEST_F(GrafoListaAdjNavegacaoTest, bfsGrafo1CompNaoPonderado) {
+	inserirVertices(grafo, 1, 9);
+	construirGrafoNaoPonderadoNaoDirecionado(grafo);
+
+	int* distancias = grafo->bfs("v1");
+	EXPECT_EQ(distancias[0], 0);
+	EXPECT_EQ(distancias[1], 1);
+	EXPECT_EQ(distancias[2], 1);
+	EXPECT_EQ(distancias[3], 2);
+	EXPECT_EQ(distancias[4], 2);
+	EXPECT_EQ(distancias[5], 3);
+	EXPECT_EQ(distancias[6], 3);
+	EXPECT_EQ(distancias[7], 4);
+	EXPECT_EQ(distancias[8], 3);
+	free(distancias);
+
+	distancias = grafo->bfs("v2");
+	EXPECT_EQ(distancias[0], 1);
+	EXPECT_EQ(distancias[1], 0);
+	EXPECT_EQ(distancias[2], 2);
+	EXPECT_EQ(distancias[3], 1);
+	EXPECT_EQ(distancias[4], 3);
+	EXPECT_EQ(distancias[5], 2);
+	EXPECT_EQ(distancias[6], 2);
+	EXPECT_EQ(distancias[7], 3);
+	EXPECT_EQ(distancias[8], 4);
+	free(distancias);
+}
+
+TEST_F(GrafoListaAdjNavegacaoTest, bfsGrafo5CompNaoPonderado) {
+	inserirVertices(grafo, 0, 17);
+	construirGrafoCom5Componentes(grafo);
+
+	int* distancias = grafo->bfs("v0");
+	EXPECT_EQ(distancias[0], 0);
+	EXPECT_EQ(distancias[1], 0);
+	EXPECT_EQ(distancias[2], 0);
+	EXPECT_EQ(distancias[3], 0);
+	EXPECT_EQ(distancias[4], 1);
+	EXPECT_EQ(distancias[5], 0);
+	EXPECT_EQ(distancias[6], 0);
+	EXPECT_EQ(distancias[7], 0);
+	EXPECT_EQ(distancias[8], 1);
+	EXPECT_EQ(distancias[9], 0);
+	EXPECT_EQ(distancias[10], 0);
+	EXPECT_EQ(distancias[11], 0);
+	EXPECT_EQ(distancias[12], 0);
+	EXPECT_EQ(distancias[13], 1);
+	EXPECT_EQ(distancias[14], 1);
+	EXPECT_EQ(distancias[15], 0);
+	EXPECT_EQ(distancias[16], 0);
+	EXPECT_EQ(distancias[17], 0);
+	free(distancias);
+
+	distancias = grafo->bfs("v1");
+	EXPECT_EQ(distancias[0], 0);
+	EXPECT_EQ(distancias[1], 0);
+	EXPECT_EQ(distancias[2], 0);
+	EXPECT_EQ(distancias[3], 0);
+	EXPECT_EQ(distancias[4], 0);
+	EXPECT_EQ(distancias[5], 1);
+	EXPECT_EQ(distancias[6], 0);
+	EXPECT_EQ(distancias[7], 0);
+	EXPECT_EQ(distancias[8], 0);
+	EXPECT_EQ(distancias[9], 0);
+	EXPECT_EQ(distancias[10], 0);
+	EXPECT_EQ(distancias[11], 0);
+	EXPECT_EQ(distancias[12], 0);
+	EXPECT_EQ(distancias[13], 0);
+	EXPECT_EQ(distancias[14], 0);
+	EXPECT_EQ(distancias[15], 0);
+	EXPECT_EQ(distancias[16], 2);
+	EXPECT_EQ(distancias[17], 2);
+	free(distancias);
+
+	distancias = grafo->bfs("v11");
+	EXPECT_EQ(distancias[0], 0);
+	EXPECT_EQ(distancias[1], 0);
+	EXPECT_EQ(distancias[2], 0);
+	EXPECT_EQ(distancias[3], 0);
+	EXPECT_EQ(distancias[4], 0);
+	EXPECT_EQ(distancias[5], 0);
+	EXPECT_EQ(distancias[6], 1);
+	EXPECT_EQ(distancias[7], 1);
+	EXPECT_EQ(distancias[8], 0);
+	EXPECT_EQ(distancias[9], 0);
+	EXPECT_EQ(distancias[10], 0);
+	EXPECT_EQ(distancias[11], 0);
+	EXPECT_EQ(distancias[12], 0);
+	EXPECT_EQ(distancias[13], 0);
+	EXPECT_EQ(distancias[14], 0);
+	EXPECT_EQ(distancias[15], 0);
+	EXPECT_EQ(distancias[16], 0);
+	EXPECT_EQ(distancias[17], 0);
+	free(distancias);
+
+	distancias = grafo->bfs("v3");
+	EXPECT_EQ(distancias[0], 0);
+	EXPECT_EQ(distancias[1], 0);
+	EXPECT_EQ(distancias[2], 2);
+	EXPECT_EQ(distancias[3], 0);
+	EXPECT_EQ(distancias[4], 0);
+	EXPECT_EQ(distancias[5], 0);
+	EXPECT_EQ(distancias[6], 0);
+	EXPECT_EQ(distancias[7], 0);
+	EXPECT_EQ(distancias[8], 0);
+	EXPECT_EQ(distancias[9], 1);
+	EXPECT_EQ(distancias[10], 3);
+	EXPECT_EQ(distancias[11], 0);
+	EXPECT_EQ(distancias[12], 0);
+	EXPECT_EQ(distancias[13], 0);
+	EXPECT_EQ(distancias[14], 0);
+	EXPECT_EQ(distancias[15], 2);
+	EXPECT_EQ(distancias[16], 0);
+	EXPECT_EQ(distancias[17], 0);
+	free(distancias);
 }
