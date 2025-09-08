75827592be834950b07afa1275036d57b916d42f
Vitor Yeso <vitor.freitas.123@ufrn.edu.br>
2025-09-08 11:15:22 -0300

    insertion sort

--- Makefile
+++ Makefile
@@ -7,6 +7,9 @@ main_data_structures: ./main_data_structures.cpp
 main_algorithms: ./main_algorithms.cpp
 	g++ -O1 -Wall -o ./bin/main_algorithms ./main_algorithms.cpp ./algorithms/cpp/luAlgorithms.cpp ./utils/utils.cpp
 
+main_lista: ./main_lista.cpp
+	g++ -O1 -Wall -o ./bin/main_lista ./main_lista.cpp
+
 clear:
 	rm ./bin/main_algorithms ./bin/main_data_structures ./bin/benchmark_visualizer
 
---
+++ main_lista.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <iterator>
+
+using namespace std;
+
+// Assinatura que você deve implementar
+void insertion_sort(vector<int>& V) {
+	if (V.size() == 1) return;
+	int i, j, left;
+	for (i=1; i<V.size(); i++) {
+		for (j=i; j>0; j--) {
+			if (V[j-1] > V[j]) {
+				left = V[j - 1];
+				// swap(V[j], V[j - 1])
+				V[j - 1] = V[j];
+				V[j] = left;
+			}
+			else break;
+		}
+	}
+}
+
+// Assinatura para implementar o merge_sort
+void merge_sort(vector<int>& V) {
+
+}
+
+static vector<int> read_vector_from_brackets_line() {
+    // Lê uma linha no formato: [10 3 5 2]
+    string line;
+    getline(cin, line);
+    while (line.size() == 0) getline(cin, line);
+    if (line.front() != '[' || line.back() != ']') {
+        cerr << "Formato incorreto. Use: [10 3 5 2]" << endl;
+        return {};
+    }
+    // remove colchetes
+    line.erase(line.begin());
+    line.pop_back();
+
+    istringstream iss(line);
+    vector<int> v{istream_iterator<int>{iss}, istream_iterator<int>{}};
+    return v;
+}
+
+static void print_vector(const vector<int>& v) {
+    cout << "[";
+    for (size_t i = 0; i < v.size(); ++i) {
+        cout << v[i];
+        if (i + 1 < v.size()) cout << " ";
+    }
+    cout << "]\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << "Digite o vetor no formato [a b c d]:\n";
+    vector<int> v = read_vector_from_brackets_line();
+
+    cout << "Vetor original: ";
+    print_vector(v);
+
+    insertion_sort(v);
+
+    cout << "Vetor ordenado:  ";
+    print_vector(v);
+
+    return 0;
+}
+
+
